7c02ab1472a5e71aa32c9b4a0832ab43b9a3e22d
AYUSH/LUCIFER <96679851+monarch21@users.noreply.github.com>
2022-02-05 21:56:27 +0530

    Update luci.ts

--- src/commands/General/luci.ts
+++ src/commands/General/luci.ts
@@ -10,7 +10,7 @@ import { MessageType } from '@adiwajshing/baileys'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'cara',
+            command: 'luci',
             description: `Get lucifer's repo`,
             aliases: ['luci'],
             category: 'general',
