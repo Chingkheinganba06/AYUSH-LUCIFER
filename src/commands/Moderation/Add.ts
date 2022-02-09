aacda357fc5adb5d4081003343652010f0f6eac2
AYUSH/LUCIFER <96679851+monarch21@users.noreply.github.com>
2022-02-09 09:22:04 +0530

    Update Add.ts

--- src/commands/Moderation/Add.ts
+++ src/commands/Moderation/Add.ts
@@ -6,6 +6,7 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             adminOnly: true,
+            modsOnly:true,
             command: 'add',
             description: 'adds participant to group chats',
             category: 'moderation',
