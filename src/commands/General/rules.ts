338ea637dfcb13e018c8201e43c378612420387b
AYUSH/LUCIFER <96679851+monarch21@users.noreply.github.com>
2022-02-09 12:13:45 +0530

    Update rules.ts

--- src/commands/General/rules.ts
+++ src/commands/General/rules.ts
@@ -1,31 +1,32 @@
-import { MessageType, Mimetype } from "@adiwajshing/baileys";
-import MessageHandler from "../../Handlers/MessageHandler";
-import BaseCommand from "../../lib/BaseCommand";
-import WAClient from "../../lib/WAClient";
-import { ISimplifiedMessage } from "../../typings";
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import axios from 'axios'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
+// import { MessageType, Mimetype } from '@adiwajshing/baileys'
 
 export default class Command extends BaseCommand {
-	constructor(client: WAClient, handler: MessageHandler) {
-		super(client, handler, {
-			command: "rules",
-			description: "FOLLOE THESE RULES IF YOU DONT WANNA BE BAN OR REMOVED ",
-			category: "general",
-			usage: `${client.config.prefix}rules`,
-			baseXp: 10,
-		});
-	}
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'rules',
+            description: `Get rules list`,
+            aliases: ['rules'],
+            category: 'general',
+            usage: `${client.config.prefix}rules`,
+            baseXp: 50
+        })
+    }
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
-		const chitoge ="https://media.tenor.com/videos/396c9bcf81fae10c25fdab6d940725d3/mp4";
-		return void this.client.sendMessage(
-			M.from,
-			{ url: chitoge },
-			MessageType.video,
-			{
-				quoted: M.WAMessage,
-				mimetype: Mimetype.gif,
-				caption🌟:`_*----🎀[Rule]🎀----*_\n\n❌*DONT ASK FOR THE SCRIPT*🚫\n*>>>* use !support to get the Official group link in your dm\n*--->*  If you want to chat with Star you can use *!tada/chat (your text)* both are different ai chat bots\n*--->* If you want to add Star Chan in your group the contact the owner by *!owner/!mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫 `,
-			}
-		);
-	};
+		return void.M.reply( await request.buffer("https://i.pinimg.com/564x/68/bd/76/68bd76132b5e35287b7cf4a7f9749f1b.jpg"),
+		MessageType.image,
+			  undefined,
+	                  undefined,
+			  `_*----🎀[Rule]🎀----*_\n\n❌*DONT ASK FOR THE SCRIPT*🚫\n*>>>* use !support to get the Official group link in your dm\n*--->*  If you want to chat with Star you can use *!tada/chat (your text)* both are different ai chat bots\n*--->* If you want to add Star Chan in your group the contact the owner by *!owner/!mods* \n*--->* Dont use wrong command, use the command given in the *help list* \n*--->* Dont spam the bot with commands if the bot is not responding, its means the bot maybe offline or facing internet issue. \n*--->* Dont Dm the bot \n\n*IF YOU DONT FOLLOW THE RULES THEN YOU WILL BE BAN SOON* 🚫 `,
+			    undefined
+                      ).catch((reason: any) =>
+                   M.reply(`✖ An error occurred. Please try again later.`))
+    }
 }
