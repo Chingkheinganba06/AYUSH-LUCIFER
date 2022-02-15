b1b0c6d006c6676db38b64d65d1c58ad75f1eb4a
AYUSH/LUCIFER <96679851+monarch21@users.noreply.github.com>
2022-02-15 20:55:16 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -2,66 +2,92 @@ import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
-
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
 export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'help',
-            description: 'Displays the help menu or shows the info of the command provided',
-            category: 'general',
-            usage: `${client.config.prefix}help (command_name)`,
-            aliases: ["h","menu","help"],
-            baseXp: 30
-        })
-    }
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "help",
+			description:
+				"Displays the help menu or shows the info of the command provided",
+			category: "general",
+			usage: `${client.config.prefix}help (command_name)`,
+			aliases: ["h"],
+			baseXp: 30,
+		});
+	}
 
-    run = async (M: ISimplifiedMessage,parsedArgs: IParsedArgs): Promise<void> => {
+	run = async (M: ISimplifiedMessage,parsedArgs: IParsedArgs): Promise<void> => {
 		const user = M.sender.jid;
-		const chitoge ="https://media.tenor.com/videos/8d1f36c507f4d7b6093d39fc1c873ee2/mp4";
-        if (!parsedArgs.joined) {
-            const commands = this.handler.commands.keys()
-            const categories: { [key: string]: ICommand[] } = {}
-            for (const command of commands) {
-                const info = this.handler.commands.get(command)
-                if (!command) continue
-                if (!info?.config?.category) continue
-                if (Object.keys(categories).includes(info.config.category)) categories[info.config.category].push(info)
-                else {
-                    categories[info.config.category] = []
-                    categories[info.config.category].push(info)
-                }
-            }
-            let text = `👋🏻 (💙ω💙) Konichiwa! *@${
+		const chitoge ="https://media.tenor.com/videos/439802e1072533f91c5d309bc7561c32/mp4";
+		if (!parsedArgs.joined) {
+			const commands = this.handler.commands.keys();
+			const categories: { [key: string]: ICommand[] } = {};
+			for (const command of commands) {
+				const info = this.handler.commands.get(command);
+				if (!command) continue;
+				if (!info?.config?.category || info.config.category === "dev") continue;
+				if (
+					!info?.config?.category ||
+					(info.config.category === "nsfw" &&
+						!(await this.client.getGroupData(M.from)).nsfw)
+				)
+					continue;
+				if (Object.keys(categories).includes(info.config.category))
+					categories[info.config.category].push(info);
+				else {
+					categories[info.config.category] = [];
+					categories[info.config.category].push(info);
+				}
+			}
+			let text = `👋🏻 (💙ω💙) Konichiwa! *@${
 				user.split("@")[0]
-			}*,━❰I'm lucifer❱━\n*it's my official group*\n*https://chat.whatsapp.com/E5CwW1dAXjRKE3XuLXxF8J*\n\n My prefix is -"${this.client.config.prefix
+			}*,━❰I'm AYUSH a whatsapp bot*\n*tell me how can i help you?❱━\n*it's my official group*\n*https://chat.whatsapp.com/E5CwW1dAXjRKE3XuLXxF8J*\n\nMy prefix is - "${
+				this.client.config.prefix
 			}"\n\nThe usable commands are listed below.\n\n`;
-            const keys = Object.keys(categories).sort((a, b) => a.localeCompare(b))
-            for (const key of keys)
-                text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
-                    key
-                ]
-                    .map((command) => command.config?.command)
-                    .join(', ')}\`\`\`\n\n`
-            return void M.reply(
-                `${text} 📚 Use ${this.client.config.prefix}help <command_name> to view the full info.\n\n🔖 Eg:/help waifu`
-            )
-        }
-        const key = parsedArgs.joined.toLowerCase()
-        const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
-        if (!command) return void M.reply(`No Command of Alias Found | "${key}"`)
-        const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
-        M.reply(
-            `*📗 Command:* ${this.client.util.capitalize(command.config?.command)}\n🎗️ *Status:* ${
-                state ? 'Disabled' : 'Available'
-            }\n🀄 *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
-                command.config.aliases && command.config.command !== 'react'
-                    ? `\n🍥 *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
-                    : ''
-            }\n🃏 *Group Only:* ${this.client.util.capitalize(
-                JSON.stringify(!command.config.dm ?? true)
-            )}\n📘 *Usage:* ${command.config?.usage || ''}\n\n📙 *Description:* ${command.config?.description || ''}`
-        )
-    }
-
-    emojis = ['🏮', '💻', '🎐', '🎍', '🔍', '🛡', '♓', '🎵', '🥇', '🔩', '🍀']
+			const keys = Object.keys(categories);
+			for (const key of keys)
+				text += `*━━━❰ ${this.client.util.capitalize(
+					key
+				)} ❱━━━*\n❐ \`\`\`${categories[key]
+					.map((command) => command.config?.command)
+					.join(" \n ")}\`\`\`\n\n`;
+			return void this.client.sendMessage(
+				M.from,
+				{ url: chitoge },
+				MessageType.video,
+				{
+					quoted: M.WAMessage,
+					mimetype: Mimetype.gif,
+					caption: `${text} 📝 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`,
+					contextInfo: { mentionedJid: [user] },
+				}
+			);
+		}
+		const key = parsedArgs.joined.toLowerCase();
+		const command =
+			this.handler.commands.get(key) || this.handler.aliases.get(key);
+		if (!command) return void M.reply(`No Command of Alias Found | "${key}"`);
+		const state = await this.client.DB.disabledcommands.findOne({
+			command: command.config.command,
+		});
+		M.reply(
+			`🚀 *Command:* ${this.client.util.capitalize(
+				command.config?.command
+			)}\n📉 *Status:* ${
+				state ? "Disabled" : "Available"
+			}\n⛩ *Category:* ${this.client.util.capitalize(
+				command.config?.category || ""
+			)}${
+				command.config.aliases
+					? `\n♦️ *Aliases:* ${command.config.aliases
+							.map(this.client.util.capitalize)
+							.join(", ")}`
+					: ""
+			}\n🎐 *Group Only:* ${this.client.util.capitalize(
+				JSON.stringify(!command.config.dm ?? true)
+			)}\n💎 *Usage:* ${command.config?.usage || ""}\n\n📒 *Description:* ${
+				command.config?.description || ""
+			}`
+		);
+	};
 }
